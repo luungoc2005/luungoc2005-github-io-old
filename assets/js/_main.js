868ebab962365ae6988b2fce5d49d49d7baa2bab
luungoc2005 <luungoc2005@gmail.com>
2017-05-03 14:20:05 +0700

    fix spacing

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -97,7 +97,7 @@ $(document).ready(function(){
     midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
   });
 
-  // init parallax
+// init parallax
 //  $('#scene-content').parallax();
 //  $('#scene').css('opacity', 1);
 });
